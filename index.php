89745b8e44c6a842350e2511f10ea7c25bedae4f
José Guilherme <guilhermepunk2@gmail.com>
2015-06-03 21:07:10 -0300

    aula 03/06

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="ISO-8859-1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
+        <script src="Js/jquery-1.11.3.js" type="text/javascript"></script>
+        <script src="Js/bootstrap.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6">
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">nome</label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">endereços</label>
+                            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail3">sexo</label>
+
+                            <label class="checkbox-inline">
+                                <input type="radio" name="sexo" value="m"> Masculino
+                                <label class="checkbox-inline">
+                                    <input type="radio" name="sexo" value="f"> Feminino
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox"> Ativo
+                                        </label>
+                                    </div>
+                                    <button type="button" class="btn btn-success">Success</button>
+                                </label>
+                            </label>
+                        </div>
+                    </form>
+                </div>
+
+
+
+
+                <div class="col-md-6">.col-md-6</div>
+            </div>
+    </body>
+</html>
+
